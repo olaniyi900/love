e6cd9c95f38d28358ab8bc4ac685f5c47694c531
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2019-05-18 16:11:02 +0100

    love

---
+++ main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector("img");
+myImage.onclick = function (){
+    var mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/dog.jpg"){
+        myImage.setAttribute("src","images/ant.jpg");
+    }else{
+        myImage.setAttribute("src","images/dog.jpg");
+    }
+}
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h1");
+function setUserName() {
+    var myName = prompt("please enter your name: ");
+    localStorage.setItem("name",myName);
+    myHeading.textContent = "You am awesome, " + myName 
+}
+if (!localStorage.getItem("name")) {
+    setUserName();
+    
+}else{
+    var storedName = localStorage.getItem("name");
+    myHeading.textContent = "You am awesome, " + storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+}
